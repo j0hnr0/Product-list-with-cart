7fe6f7a0569646109427a29dbb4391054d614e42
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-03-01 22:03:56 +0800

    Image optimization and responsiveness

--- app/_components/product-section.js
+++ app/_components/product-section.js
@@ -3,7 +3,7 @@ import Product from "./product";
 export default function ProductSection() {
   return (
     <section>
-      <h1 className="text-[40px] font-bold font-red-hat-bold">Dessert</h1>
+      <h1 className="text-[40px] font-bold font-red-hat-bold">Desserts</h1>
       <div className="flex gap-6">
         <Product />
         <Product />
--- app/_components/product.js
+++ app/_components/product.js
@@ -6,10 +6,11 @@ export default function Product({ imageURL, category, name, price, alt }) {
       <div>
         <Image
           src="/images/image-waffle-desktop.jpg"
+          className="w-full h-auto max-w-[250px] rounded-lg"
+          alt="test"
           width={250}
           height={240}
-          className="rounded-lg"
-          alt="test"
+          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
         />
       </div>
     </div>
