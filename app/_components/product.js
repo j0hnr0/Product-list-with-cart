86a08c1db586a962434646231444445199b90bbb
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-03-03 22:42:40 +0800

    added Add to cart button

--- app/_components/product.js
+++ app/_components/product.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Product({ imageURL, category, name, price, alt }) {
   return (
     <div>
-      <div>
+      <div className="relative">
         <Image
           src="/images/image-waffle-desktop.jpg"
           className="w-full h-auto max-w-[250px] rounded-lg"
@@ -12,6 +12,17 @@ export default function Product({ imageURL, category, name, price, alt }) {
           height={240}
           sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
         />
+        <div className="w-40 absolute bottom-[-25px] left-[50%] translate-x-[-50%] z-10 rounded-full flex justify-center items-center gap-2 border-[1px] border-[#AD8A85] bg-white py-3">
+          <Image
+            src="/images/icon-add-to-cart.svg"
+            alt="A cart icon"
+            width={20}
+            height={20}
+          />
+          <h3 className="font-red-hat-semibold font-semibold text-sm">
+            Add to Cart
+          </h3>
+        </div>
       </div>
     </div>
   );
