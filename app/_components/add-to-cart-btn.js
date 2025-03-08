03709708b856be376d47b00debfa834038091f5f
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-03-08 16:26:58 +0800

    added minus icon functionality

--- app/_components/add-to-cart-btn.js
+++ app/_components/add-to-cart-btn.js
@@ -7,51 +7,68 @@ import { useState } from "react";
 export default function AddToCartButton() {
   const [isClicked, setIsClicked] = useState(false);
 
-  function handleClick() {
-    setIsClicked((prev) => !prev);
+  function buyProduct() {
+    setIsClicked(true);
+  }
+
+  function reduceAmountClick() {
+    setIsClicked(false);
   }
 
   return (
     <button
       type="button"
-      onClick={handleClick}
+      onClick={buyProduct}
       className={clsx(
         `w-40 absolute bottom-[-25px] left-[50%] translate-x-[-50%] z-10 rounded-full 
-         border-[1px] py-3 cursor-pointer
+         border-[1px] py-3
         hover:border-custom-red hover:text-custom-red`,
         {
-          "bg-custom-red border-custom-red": isClicked === true,
-          "bg-white border-[#AD8A85]": isClicked === false,
+          "bg-custom-red border-custom-red cursor-default": isClicked === true,
+          "bg-white border-[#AD8A85] cursor-pointer": isClicked === false,
         }
       )}
     >
       {isClicked && (
-        <div className="flex justify-between items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="10"
-            height="2"
-            fill="none"
-            viewBox="0 0 10 2"
+        <div className="flex justify-between items-center px-3">
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              reduceAmountClick();
+            }}
+            className="group flex justify-center items-center rounded-full bg-transparent hover:bg-white border-[1px] border-white w-5 h-5 cursor-pointer"
           >
-            <path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="10"
+              height="2"
+              fill="none"
+              viewBox="0 0 10 2"
+            >
+              <path
+                className="fill-white group-hover:fill-custom-red"
+                d="M0 .375h10v1.25H0V.375Z"
+              />
+            </svg>
+          </div>
 
           <h3 className="font-red-hat-semibold font-semibold text-sm text-white">
             1
           </h3>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="10"
-            height="10"
-            fill="none"
-            viewBox="0 0 10 10"
-          >
-            <path
-              fill="#fff"
-              d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
-            />
-          </svg>
+          <div className="group flex justify-center items-center rounded-full bg-transparent hover:bg-white border-[1px] border-white w-5 h-5 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="10"
+              height="10"
+              fill="none"
+              viewBox="0 0 10 10"
+            >
+              <path
+                className="fill-white group-hover:fill-custom-red"
+                d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+              />
+            </svg>
+          </div>
         </div>
       )}
 
