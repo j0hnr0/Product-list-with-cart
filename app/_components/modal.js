9bb5eb0041b3e290ac4a3500066e45c12d1fdc09
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-03-18 00:45:00 +0800

    added modal component

---
+++ app/_components/modal.js
@@ -0,0 +1,7 @@
+export default function Modal() {
+    return <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/50">
+        <div className="bg-white max-w-[592px] rounded-xl p-[40px]">
+
+        </div>
+    </div>
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Modal from "./_components/modal";
 import ProductSection from "./_components/product-section";
 import YourCartSection from "./_components/your-cart-section";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="max-w-[1216px] mx-auto flex justify-between items-start pt-10">
       <ProductSection />
       <YourCartSection />
+      <Modal />
     </main>
   );
 }
